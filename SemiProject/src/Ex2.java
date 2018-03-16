7afd0296c09808a91358b05350ee9c63595da55f
user2 <user2@user2-PC>
2018-03-16 18:05:13 +0900

    //종료까지 수정

--- SemiProject/src/Ex2.java
+++ SemiProject/src/Ex2.java
@@ -1,39 +1,37 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Ex2 extends JFrame implements KeyListener{
+public class Ex2 extends JFrame implements KeyListener {
 	String sol = null;
-	String[] images = {"up.PNG", "left.PNG", "down.PNG", "right.PNG"};
-//	JTextField[] l_name = new JTextField[8];
-	JLabel[] l_name = new JLabel[8];
-	 JPanel jp;
-	 int count = 0;
+	String[] images = { "up.PNG", "left.PNG", "down.PNG", "right.PNG" };
+	JLabel[] l_name = new JLabel[15];
+	static Ex2 ex2 = new Ex2();
+	JPanel jp;
+	static int count = 0;
+	static int count2 = 1;
 
 	public static void main(String[] args) {
-			new Ex2();
+		ex2.run();
 	}
-	
-	public Ex2(){
-		
-		setSize(500,300);
-		setTitle("미니게임1");
-		
-		jp =  new JPanel();
-		jp.setLayout(new GridLayout(2,4,10,10));
 
-		int num=0;
+	public void run() {
+
+		setSize(700, 400);
+		setTitle("미니게임1");
+
+		jp = new JPanel();
+		jp.setLayout(new GridLayout(3, 5, 10, 10));
+
+		int num = 0;
 		int ranNum = 0;
 		String str = "";
-		while(num<8){
-			ranNum = (int)(Math.random()*4);
-			str += ranNum+"";
+		while (num < 15) {
+			ranNum = (int) (Math.random() * 4);
+			str += ranNum + "";
 			jp.add(l_name[num] = new JLabel(new ImageIcon(images[ranNum])));
-//			jp.add(l_name[num] = new JTextField(ranNum+""));
-			System.out.println(l_name[num]);
 			l_name[num].addKeyListener(this);
 			num++;
 		}
@@ -41,66 +39,109 @@ public class Ex2 extends JFrame implements KeyListener{
 
 		add(jp);
 		setVisible(true);
-		for(int i=0; i<l_name.length; i++){
+		for (int i = 0; i < l_name.length; i++) {
 			l_name[i].requestFocus();
 		}
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
 
-			char solNum = sol.charAt(count);
-			switch(e.getKeyCode()){
-			case KeyEvent.VK_UP :
-				System.out.println(sol);
-				if(solNum=='0'){
-//					l_name[count].setText("정답");
-					l_name[count].setIcon(new ImageIcon("tiger.PNG"));
-					count++;
+		char solNum = sol.charAt(count);
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+
+			if (solNum == '0') {
+				l_name[count].setIcon(new ImageIcon("tiger.PNG"));
+				count++;
+				if (count >= 15) {
+					JOptionPane.showMessageDialog(null, "남은 횟수는 "+(3-count2)+"회 입니다.");
+					count = 0;
+					count2++;
+					run();
 				}
-				break;
-			case KeyEvent.VK_LEFT :
-				if(solNum=='1'){
-//					l_name[count].setText("정답");
-					l_name[count].setIcon(new ImageIcon("tiger.PNG"));
-					count++;
+				if(count2 >=4){
+					JOptionPane.showMessageDialog(null, "게임이 종료되었습니다.");
+					System.exit(0);
 				}
-				break;
-			case KeyEvent.VK_DOWN :
-				if(solNum=='2'){
-//					l_name[count].setText("정답");
-					l_name[count].setIcon(new ImageIcon("tiger.PNG"));
-					count++;
-				}
-				break;
-			case KeyEvent.VK_RIGHT :
-				if(solNum=='3'){
-//					l_name[count].setText("정답");
-					l_name[count].setIcon(new ImageIcon("tiger.PNG"));
-					count++;
-				}
-				break;
-			default : System.out.println("방향키를 입력하세요.");
 			}
 
+			break;
+		case KeyEvent.VK_LEFT:
+
+			if (solNum == '1') {
+				l_name[count].setIcon(new ImageIcon("tiger.PNG"));
+				count++;
+				if (count >= 15) {
+					JOptionPane.showMessageDialog(null, "남은 횟수는 "+(3-count2)+"회 입니다.");
+					count = 0;
+					count2++;
+					run();
+				}
+				if(count2 >=4){
+					JOptionPane.showMessageDialog(null, "게임이 종료되었습니다.");
+					System.exit(0);
+				}
+			}
+
+			break;
+		case KeyEvent.VK_DOWN:
+
+			if (solNum == '2') {
+				l_name[count].setIcon(new ImageIcon("tiger.PNG"));
+				count++;
+				if (count >= 15) {
+					JOptionPane.showMessageDialog(null, "남은 횟수는 "+(3-count2)+"회 입니다.");
+					count = 0;
+					count2++;
+					run();
+				}
+				if(count2 >=4){
+					JOptionPane.showMessageDialog(null, "게임이 종료되었습니다.");
+					System.exit(0);
+				}
+			}
+
+			break;
+		case KeyEvent.VK_RIGHT:
+
+			if (solNum == '3') {
+				l_name[count].setIcon(new ImageIcon("tiger.PNG"));
+				count++;
+			}
+			if (count >= 15) {
+				JOptionPane.showMessageDialog(null, "남은 횟수는 "+(3-count2)+"회 입니다.");
+				count = 0;
+				count2++;
+				run();
+			}
+			if(count2 >=4){
+				JOptionPane.showMessageDialog(null, "게임이 종료되었습니다.");
+				System.exit(0);
+			}
+
+			break;
+		default:
+			System.out.println("방향키를 입력하세요.");
+		}
+
 		System.out.println("키 입력 완료");
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 }
