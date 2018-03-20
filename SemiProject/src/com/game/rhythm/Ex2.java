17dec8202b00f4ecd89b251fbdef61ab3bdeeef8
Seung Won Choi <yuventius125@icloud.com>
2018-03-20 21:16:37 +0900

    life image 수정

--- SemiProject/src/com/game/rhythm/Ex2.java
+++ SemiProject/src/com/game/rhythm/Ex2.java
@@ -2,14 +2,12 @@ package com.game.rhythm;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -24,17 +22,18 @@ public class Ex2 extends JFrame implements KeyListener {
 	static Ex2 ex2 = new Ex2(); // 자신의 객체 선언
 	static int life = 3; // 라이프
 	JPanel jp; // 그리드레이아웃을 담을 패널
-	JPanel base;//가장 아래 베이스 패널-보더레이아웃
-	JPanel bar;//게임 화면 위에 위치-> 타이머,남은 생명 나타냄
-	JPanel barbase; //그리드로 생명 줄어도 나머지 라벨 위치 이동 없게
-	JPanel lifePanel;//생명 이미지 넣을 패널
+	JPanel base;// 가장 아래 베이스 패널-보더레이아웃
+	JPanel bar;// 게임 화면 위에 위치-> 타이머,남은 생명 나타냄
+	JPanel barbase; // 그리드로 생명 줄어도 나머지 라벨 위치 이동 없게
+	JPanel lifePanel;// 생명 이미지 넣을 패널
 	static int count = 0; // 입력 횟수
 	static int count2 = 1; // 게임 반복 횟수
-	static int time = 20; //게임 제한 초
+	static int time = 20; // 게임 제한 초
 	JLabel timeLabel;
-	JLabel lifeLabel1, lifeLabel2, lifeLabel3;//생명 하트 1,2,3
+	JLabel lifeLabel1, lifeLabel2, lifeLabel3;// 생명 하트 1,2,3
 	JProgressBar timeP;
-	Image dieImage; //생명 줄어들때 대체 이미지(바탕색)
+	Image dieImage; // 생명 줄어들때 대체 이미지(바탕색)
+	static Timer timer;
 
 	// 실행용 메인메소드
 	public static void main(String[] args) {
@@ -45,12 +44,11 @@ public class Ex2 extends JFrame implements KeyListener {
 	public void gameRun() {
 
 		setSize(700, 400);
-		setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]");
-		
-		base = new JPanel(new BorderLayout());
-		bar = new JPanel(new GridLayout(1,2,1,1));
-		barbase = new  JPanel(new FlowLayout(FlowLayout.RIGHT));
+		setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : " + count2 + "번]");
 
+		base = new JPanel(new BorderLayout());
+		bar = new JPanel(new GridLayout(1, 2, 1, 1));
+		barbase = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 
 		bar.add(timeP = new JProgressBar());
 		timeP.setMinimum(0);
@@ -61,24 +59,28 @@ public class Ex2 extends JFrame implements KeyListener {
 
 		bar.add(lifePanel = new JPanel());
 		barbase.add(bar);
-		
+
 		lifePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-		
+
 		lifePanel.add(lifeLabel1 = new JLabel());
 		lifePanel.add(lifeLabel2 = new JLabel());
 		lifePanel.add(lifeLabel3 = new JLabel());
-		
+
 		Image lifeImage = new ImageIcon("lifeImage.PNG").getImage().getScaledInstance(20, 20, 0);
 		dieImage = new ImageIcon("baseImg.PNG").getImage().getScaledInstance(20, 20, 0);
-		lifeLabel1.setIcon(new ImageIcon(lifeImage));
-		lifeLabel2.setIcon(new ImageIcon(lifeImage));
-		lifeLabel3.setIcon(new ImageIcon(lifeImage));
-
+		if (life == 3) {
+			lifeLabel1.setIcon(new ImageIcon(lifeImage));
+			lifeLabel2.setIcon(new ImageIcon(lifeImage));
+			lifeLabel3.setIcon(new ImageIcon(lifeImage));
+		} else if (life == 2) {
+			lifeLabel2.setIcon(new ImageIcon(lifeImage));
+			lifeLabel3.setIcon(new ImageIcon(lifeImage));
+		} else if (life == 1) {
+			lifeLabel3.setIcon(new ImageIcon(lifeImage));
+		}
 
 		jp = new JPanel();
 		jp.setLayout(new GridLayout(3, 5, 10, 10));
-		
-
 
 		int num = 0;
 		int ranNum = 0;
@@ -91,17 +93,17 @@ public class Ex2 extends JFrame implements KeyListener {
 			num++;
 		}
 		sol = str;
-		
+
 		base.add(jp, BorderLayout.CENTER);
 		base.add(barbase, BorderLayout.NORTH);
-		
+
 		add(base);
 
 		setVisible(true);
 		for (int i = 0; i < l_name.length; i++) {
 			l_name[i].requestFocus();
 		}
-		if(time==20){
+		if (time == 20) {
 			new Timer().start();
 		}
 
@@ -124,26 +126,24 @@ public class Ex2 extends JFrame implements KeyListener {
 
 				if (count >= 15) {
 					JOptionPane.showMessageDialog(null, "남은 횟수는 " + (3 - count2) + "회 입니다.");
-					
 					count = 0;
 					count2++;
-					if (count2 >= 4) {			//마지막 run을 실행 시키지 않음
+					if (count2 >= 4) { // 마지막 run을 실행 시키지 않음
 						JOptionPane.showMessageDialog(null, "게임이 종료되었습니다.");
 						System.exit(0);
 					}
 					gameRun();
-					setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]");	//게임횟수 초기화를 위한 구문
-
+					setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : " + count2 + "번]"); // 게임횟수 초기화를 위한 구문
 				}
 			} else { // 틀릴경우
 				life--;
 
-				if(life==2){
+				if (life == 2) {
 					lifeLabel3.setIcon(new ImageIcon(dieImage));
-				}else if(life==1){
+				} else if (life == 1) {
 					lifeLabel2.setIcon(new ImageIcon(dieImage));
 				}
-				setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]"); // title은 자동으로 바뀌지 않아서 다시 setTitle
+				setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : " + count2 + "번]"); // title은 자동으로 바뀌지 않아서 다시 setTitle
 				if (life == 0) { // 라이프가 0일 경우
 					lifeLabel1.setIcon(new ImageIcon(dieImage));
 					JOptionPane.showMessageDialog(null, "라이프를 모두 소진하셨습니다.");
@@ -167,18 +167,18 @@ public class Ex2 extends JFrame implements KeyListener {
 						System.exit(0);
 					}
 					gameRun();
-					setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]");
+					setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : " + count2 + "번]");
 
 				}
 			} else {
 				life--;
 
-				if(life==2){
+				if (life == 2) {
 					lifeLabel3.setIcon(new ImageIcon(dieImage));
-				}else if(life==1){
+				} else if (life == 1) {
 					lifeLabel2.setIcon(new ImageIcon(dieImage));
 				}
-				setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]");
+				setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : " + count2 + "번]");
 
 				if (life == 0) {
 					lifeLabel1.setIcon(new ImageIcon(dieImage));
@@ -202,17 +202,17 @@ public class Ex2 extends JFrame implements KeyListener {
 						System.exit(0);
 					}
 					gameRun();
-					setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]");
+					setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : " + count2 + "번]");
 				}
 			} else {
 				life--;
 
-				if(life==2){
+				if (life == 2) {
 					lifeLabel3.setIcon(new ImageIcon(dieImage));
-				}else if(life==1){
+				} else if (life == 1) {
 					lifeLabel2.setIcon(new ImageIcon(dieImage));
 				}
-				setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]");
+				setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : " + count2 + "번]");
 				if (life == 0) {
 					lifeLabel1.setIcon(new ImageIcon(dieImage));
 					JOptionPane.showMessageDialog(null, "라이프를 모두 소진하셨습니다.");
@@ -236,18 +236,18 @@ public class Ex2 extends JFrame implements KeyListener {
 						System.exit(0);
 					}
 					gameRun();
-					setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]");
+					setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : " + count2 + "번]");
 
 				}
 			} else {
 				life--;
 
-				if(life==2){
+				if (life == 2) {
 					lifeLabel3.setIcon(new ImageIcon(dieImage));
-				}else if(life==1){
+				} else if (life == 1) {
 					lifeLabel2.setIcon(new ImageIcon(dieImage));
 				}
-				setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : "+count2+"번]");
+				setTitle("리듬게임/ [남은 라이프 : " + life + "개] [게임횟수 : " + count2 + "번]");
 				if (life == 0) {
 					lifeLabel1.setIcon(new ImageIcon(dieImage));
 					JOptionPane.showMessageDialog(null, "라이프를 모두 소진하셨습니다.");
@@ -274,12 +274,12 @@ public class Ex2 extends JFrame implements KeyListener {
 		// TODO Auto-generated method stub
 
 	}
-	
-	class Timer extends Thread{
+
+	class Timer extends Thread {
 		@Override
-		public void run(){
+		public void run() {
 			time = 20;
-			while(true){
+			while (true) {
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
@@ -287,7 +287,7 @@ public class Ex2 extends JFrame implements KeyListener {
 					e.printStackTrace();
 				}
 				time--;
-				if(time==0){
+				if (time == 0) {
 
 					timeP.setValue(time);
 					JOptionPane.showMessageDialog(null, "게임이 종료되었습니다.");
