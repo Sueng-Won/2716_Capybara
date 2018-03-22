b8563c5def9aafa5071847ff65e5e4a10794f2e2
user2 <user2@user2-PC>
2018-03-22 17:49:20 +0900

    //자동 줄바꿈 추가

--- SemiProject/src/com/game/cording/Quiz2.java
+++ SemiProject/src/com/game/cording/Quiz2.java
@@ -1,35 +1,76 @@
 package com.game.cording;
 
+
 import java.awt.Color;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.WindowConstants;
 import javax.swing.border.LineBorder;
 
+import com.game.cording.Quiz1.Timer;
+
 public class Quiz2 {
+	static int time = 60;
+	JProgressBar timeP; // 타이머 바
+	JFrame frame;
+	JPanel bar;
+	Timer timer;
+	JTextArea answerArea;
+
 	public void Quiz2() {
-		JFrame frame = new JFrame("Quiz2");
+
+		//문제창 기본 프레임
+		frame = new JFrame("Quiz2");
 		frame.setBounds(200, 200, 700, 700);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setLayout(null);
 		Font font = new Font("arian", Font.BOLD, 15);
 
-		JButton jButton = new JButton("Check the answer!");
-		frame.add(jButton, "South");
+		//타이머 바 넣을 공간 생성
+		bar = new JPanel();
+		bar.setLayout(null);
+		bar.setBounds(0, 0, 700, 30);
 
-		JTextArea jTextArea = new JTextArea();
-		jTextArea.setFont(font);
-		jTextArea.setEditable(false);
+		//바에 타이머바 삽입
+		bar.add(timeP = new JProgressBar(0,time));
+		
+		//타이머 바 정보
+		timeP.setValue(time);
+		timeP.setForeground(Color.DARK_GRAY);
+		timeP.setBorderPainted(false);
+		timeP.setBounds(0, 0, 700, 30);
 
+		frame.add(bar);
+		
+		//정답버튼 생성
+		JButton answerBtn = new JButton("OK!");
+		frame.add(answerBtn);
+		answerBtn.setBounds(290, 605, 100, 50);
+
+		//퀴즈띄울 공간 생성
+		JTextArea quizArea = new JTextArea();
+		quizArea.setBounds(0, 30, 700, 370);
+		quizArea.setFont(font);
+		quizArea.setEditable(false);
+		quizArea.setBackground(new Color(238, 238, 238));
+		frame.add(quizArea);
+
+
+		//문제 파일로 읽어오기
 		File file = new File("Quiz2.txt");
 
 		try {
@@ -38,40 +79,101 @@ public class Quiz2 {
 			StringBuilder stringBuilder = new StringBuilder();
 			String ls = System.getProperty("line.separator");
 
-			while ((line = reader.readLine()) != null){
-					stringBuilder.append(line);
-					stringBuilder.append(ls);
-
-				jTextArea.setText(stringBuilder.toString());
+			while ((line = reader.readLine()) != null) {
+				stringBuilder.append(line);
+				stringBuilder.append(ls);
+				quizArea.setText(stringBuilder.toString());
 			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
-		JTextArea jTextArea1 = new JTextArea();
-		jTextArea1.setBounds(0, 420, 680, 200);
-		jTextArea1.setBorder(new LineBorder(Color.BLACK, 3));
-		jTextArea1.setFont(font);
+		//답안 공간 생성
+		answerArea = new JTextArea();
+		
+		JScrollPane scrollP = new JScrollPane(answerArea);
+		scrollP.setBounds(0, 420, 680, 180);
 
-		frame.add(jTextArea1, "Center");
+		answerArea.setBorder(new LineBorder(Color.BLACK, 3));
+		answerArea.setFont(font);
+		answerArea.setLineWrap(true);
 
-		frame.add(jTextArea, "Center");
+		frame.add(scrollP);
+		//답안버튼 클릭시 발생하는 이벤트
+		answerBtn.addActionListener(new ActionListener() {
 
-		jButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (jTextArea1.getText().trim().equals("intArr[i].length;")) {
+				if (answerArea.getText().trim().equals("intArr[i].length;")) {
 					JOptionPane.showMessageDialog(frame, "Correct!");
 					frame.setVisible(false);
-					Quiz3 q3 =new Quiz3();
-					q3.Quiz3();
+					//랜덤으로 다음문제 제출
+					int rnum = (int) (Math.random() * 8) + 3;
+					if (rnum == 3) {
+						Quiz3 q3 = new Quiz3();
+						q3.Quiz3();
+					} else if (rnum == 4) {
+						Quiz4 q4 = new Quiz4();
+						q4.Quiz4();
+					} else if (rnum == 5) {
+						Quiz5 q5 = new Quiz5();
+						q5.Quiz5();
+					} else if (rnum == 6) {
+						Quiz6 q6 = new Quiz6();
+						q6.Quiz6();
+					} else if (rnum == 7) {
+						Quiz7 q7 = new Quiz7();
+						q7.Quiz7();
+					} else if (rnum == 8) {
+						Quiz8 q8 = new Quiz8();
+						q8.Quiz8();
+					} else if (rnum == 9) {
+						Quiz9 q9 = new Quiz9();
+						q9.Quiz9();
+					} else if (rnum == 10) {
+						Quiz10 q10 = new Quiz10();
+						q10.Quiz10();
+					}
 				} else {
 					JOptionPane.showMessageDialog(frame, "Wrong!");
 				}
+
 			}
 		});
 		frame.setVisible(true);
+		//타이머 바 호출
+
+		timer = new Timer();
+		timer.start();
 
 	}
+
+	class Timer extends Thread {
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					Thread.sleep(1000); //1초
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				time--;
+				
+				if(answerArea.getText().trim().equals("intArr[i].length;")){
+					break;
+				}
+				//0초일때 게임 종료
+				if (time == 0) {
+				//	timeP.setValue(time);
+					JOptionPane.showMessageDialog(null, "게임이 종료되었습니다.");
+					System.exit(0);
+					break;
+				}
+
+				timeP.setValue(time);
+			}
+		}
+	}
+
 }
