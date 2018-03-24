9eb277f9280e48e17edfc244ba7db31a0ae43d94
SEJUN <SEJUN@DESKTOP-LQKQG3O>
2018-03-24 20:11:57 +0900

    점수판

--- SemiProject/src/com/global/login/DataIo.java
+++ SemiProject/src/com/global/login/DataIo.java
@@ -1,16 +1,24 @@
 package com.global.login;
 
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import java.util.TreeMap;
 
-
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 public class DataIo {
@@ -73,4 +81,51 @@ public class DataIo {
 		
 		return checkIdx;
 	}
-}
+	public void scoreboard(){
+		JFrame scoreboard = new JFrame("점수판");
+		scoreboard.setBounds(600, 200, 400, 700);
+		scoreboard.setLayout(new GridLayout(5,1));
+		
+		
+		Properties readProp = new Properties();
+		try { 
+			readProp.loadFromXML(new FileInputStream("data.xml"));
+			Map mapReverse = new TreeMap(Collections.reverseOrder());
+			
+
+			Set<Object> keySet = readProp.keySet();
+			Iterator<Object> iterator = keySet.iterator();
+			List<Object> keyList = new ArrayList<Object>();
+			
+			
+			while(iterator.hasNext()){
+				String key = (String)iterator.next();
+				mapReverse.put(readProp.getProperty(key), key);
+				keyList.add(readProp.getProperty(key));
+			}
+			Collections.reverse(keyList);
+			System.out.println(mapReverse);
+			System.out.println(keyList);
+	
+			
+			for(int i=0; i<keyList.size(); i++){
+				if(i>5){
+					break;
+				}
+				String score = (String) keyList.get(i);
+				String id = (String) mapReverse.get(keyList.get(i));
+				scoreboard.add(new JLabel(i+1+"등!!"+"       "+id+"            "+score)).setFont(new Font("맑은고딕", Font.BOLD, 30));
+			}
+			
+			
+			scoreboard.setVisible(true);
+		} catch (InvalidPropertiesFormatException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		  }
+		}
+	}
+
